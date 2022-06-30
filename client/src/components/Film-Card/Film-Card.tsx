d057b6d0fbafcda69c1912beed9db8bb17541584
David <spliatzoff@gmail.com>
2022-06-30 22:54:52 +0400

    icons for various browsers

--- client/src/components/Film-Card/Film-Card.tsx
+++ client/src/components/Film-Card/Film-Card.tsx
@@ -54,18 +54,23 @@ const FilmCard: React.FC<Film> = ({ vote_average, poster_path, title, id }) => {
             <div className="card-info">
                 <div className="rate-and-buttons">
                     <p>Rate: {vote_average}</p>
-                    <button className="star" onClick={onFavouriteHandler}>
-                        {(isFavourite && '⭐') || '☆'}
+                    <button className="star-btn" onClick={onFavouriteHandler}>
+                        <i className={isFavourite ? 'star-icon star-icon-fill' : 'star-icon'} />
                     </button>
                     <button className="bookmark" onClick={onWatchLaterHandler}>
-                        <img
-                            src={
-                                (isWatchLater &&
-                                    'https://www.shareicon.net/data/2015/12/14/687209_bookmark_512x512.png') ||
-                                'https://cdn-icons-png.flaticon.com/512/709/709496.png'
-                            }
-                            alt="no photo :("
-                        />
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="25"
+                            height="25"
+                            fill="currentColor"
+                            className="bi bi-bookmark-fill"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"
+                                fill={isWatchLater ? 'rgb(93, 81, 173)' : 'rgb(44, 43, 52)'}
+                            />
+                        </svg>
                     </button>
                 </div>
                 <h3>{title}</h3>
